f9787301ea8fe07f01fe84b7bec268c96370cd75
davidwang7777 <76135199+davidwang7777@users.noreply.github.com>
2023-11-19 17:14:14 -0700

    Add files via upload

--- polideck-client/src/App.test.js
+++ polideck-client/src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+//import App from './App';
+import setEthRate, convertUsdToEth from './App';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('USD to Eth conversion Component', () => {
+
+  test('ethrate is 0 so result is supposed to be 0', () => {
+    // Arrange
+    let usdAmount = 100;
+    setEthRate(0);
+
+    // Act
+    const result = convertUsdToEth(usdAmount);
+
+    // Assert
+    expect(result).toBe(0);
+  });
+
+  test('a pass case for eth amount', () => {
+    // Arrange
+    const usdAmount = 100;
+    const ethRate = 200; // Change this to your desired rate
+
+    // Act
+    const result = convertUsdToEth(usdAmount);
+
+    // Assert
+    expect(result).toBeCloseTo(usdAmount / ethRate, 5);
+  });
 });
